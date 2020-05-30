9ee10c2b50f45234a9b13d14e8cea9cd99075fe5
SaiAshish9 <saiashish3760@gmail.com>
2020-05-31 03:24:49 +0530

    added custom relationships

--- graphql-prisma/src/prisma.js
+++ graphql-prisma/src/prisma.js
@@ -7,98 +7,98 @@ const prisma = new Prisma({
 })
 
 
-prisma.query.users(null,'{name  } ')
-.then(data=>{
-    console.log(JSON.stringify(data,undefined,3))
-    })
+// prisma.query.users(null,'{name  } ')
+// .then(data=>{
+//     console.log(JSON.stringify(data,undefined,3))
+//     })
 // replacer fn => undefined
 
-prisma.mutation.createPost({
-    data:{
-       title:'new graphql post' ,
-       published:true,
-       body:'body',
-       author:{
-           connect:{
-               id:"ckas95gpu004y0746o2acqze5"
-           }
-       }
-    }
-},'{ id title body published  }')
-.then(data=>{
-    console.log(JSON.stringify(data,undefined,3))
+// prisma.mutation.createPost({
+//     data:{
+//        title:'new graphql post' ,
+//        published:true,
+//        body:'body',
+//        author:{
+//            connect:{
+//                id:"ckas95gpu004y0746o2acqze5"
+//            }
+//        }
+//     }
+// },'{ id title body published  }')
+// .then(data=>{
+//     console.log(JSON.stringify(data,undefined,3))
 
-return prisma.query.users(null,'{ id name email posts{ id title }}')
-})
-.then(data=>{
-    console.log(JSON.stringify(data,undefined,3))
-    })
-
-
-prisma.mutation.updatePost({
-    where:{
-    id:"ckas7jml1003u07465122nu8r"
-},
-data:{
-    body:"any thing",
-    published:true
-}
-}," { id body }  ").then(data=>{
-    return prisma.query.posts(null,'{ id title body published }')
-})
-.then(data=>{
-        console.log(JSON.stringify(data,undefined,3))
-})
+// return prisma.query.users(null,'{ id name email posts{ id title }}')
+// })
+// .then(data=>{
+//     console.log(JSON.stringify(data,undefined,3))
+//     })
 
 
-const createPostForUser = async (authorId,data)=>{
+// prisma.mutation.updatePost({
+//     where:{
+//     id:"ckas7jml1003u07465122nu8r"
+// },
+// data:{
+//     body:"any thing",
+//     published:true
+// }
+// }," { id body }  ").then(data=>{
+//     return prisma.query.posts(null,'{ id title body published }')
+// })
+// .then(data=>{
+//         console.log(JSON.stringify(data,undefined,3))
+// })
 
-const userExists=await prisma.exists.User({
-   id:authorId
-})
 
-if(!userExists){
-   throw new Error('User does not exist')
-}
+// const createPostForUser = async (authorId,data)=>{
 
-   const post = await prisma.mutation.createPost({
-   data:{
-      ...data,
-      author:{
-         connect:{
-            id:authorId
-         }
-      }
-   }
-},' {id} ') 
-const user = await prisma.query.user({
-where:{
-   id:authorId
-}
-},' { id name email posts { id title published } } ')
-return user
-}
+// const userExists=await prisma.exists.User({
+//    id:authorId
+// })
 
-createPostForUser('ckas95gpu004y0746o2acqze5',{
-   title:"Great",
-   body:"The War of Art",
-   published:true
-})
-.then((user)=>{
-   console.log(JSON.stringify(user,undefined,2));
-})
-.catch(e=>{
-   console.log(e)
-})
+// if(!userExists){
+//    throw new Error('User does not exist')
+// }
 
-prisma.exists.Comment({
-   id:'ckas9fxjy005t0746y445osu6',
-   author:{
-        id:'ckas95gpu004y0746o2acqze5'
-   }
-}).then(exists=>{
-   console.log(exists)
-})
+//    const post = await prisma.mutation.createPost({
+//    data:{
+//       ...data,
+//       author:{
+//          connect:{
+//             id:authorId
+//          }
+//       }
+//    }
+// },' {id} ') 
+// const user = await prisma.query.user({
+// where:{
+//    id:authorId
+// }
+// },' { id name email posts { id title published } } ')
+// return user
+// }
+
+// createPostForUser('ckas95gpu004y0746o2acqze5',{
+//    title:"Great",
+//    body:"The War of Art",
+//    published:true
+// })
+// .then((user)=>{
+//    console.log(JSON.stringify(user,undefined,2));
+// })
+// .catch(e=>{
+//    console.log(e)
+// })
+
+// prisma.exists.Comment({
+//    id:'ckas9fxjy005t0746y445osu6',
+//    author:{
+//         id:'ckas95gpu004y0746o2acqze5'
+//    }
+// }).then(exists=>{
+//    console.log(exists)
+// })
 
 
 const updatePostForUser = async (postId,data)=>{
